15ea2645d1c946674902d47ab183eec15e59576b
Abhinavexists <abhinavkumarsingh2023@gmail.com>
2025-10-05 00:17:31 +0530

    tb global instance -> local instance removing always None Traceback issue -> logging,py

--- src/exception.py
+++ src/exception.py
@@ -8,17 +8,21 @@ from typing import Optional
 # so type checkers complain because None is not allowed in the signature.
 # Need to allow Optional[TracebackType] to handle both TraceBack and None
 
-tb = sys.exc_info()[2]
+def error_message_details(error: Exception, error_details: Optional[TracebackType]) -> str:
+    if error_details is None:
+       error_details = sys.exc_info()[2]
 
-def error_message_details(error: Exception, error_details: Optional[TracebackType] = tb) -> str:
-    file_name = error_details.tb_frame.f_code.co_filename if error_details else None
-    line_no = error_details.tb_lineno if error_details else None
+    if error_details:
+       file_name = error_details.tb_frame.f_code.co_filename
+       line_no = error_details.tb_lineno
+    else:
+        file_name = 'Unknown'
+        line_no = -1
 
-    error_message = "Error found in script [{0}] in line [{1}], Error message is [{2}]".format(file_name, line_no, str(error))
-    return error_message
+    return "Error found in script [{0}] in line [{1}], Error message is [{2}]".format(file_name, line_no, str(error))
 
 class CustomException(Exception):
-    def __init__(self, error_message: Exception, error_detail: Optional[TracebackType] = tb):
+    def __init__(self, error_message: Exception, error_detail: Optional[TracebackType]):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_details=error_detail)
 
