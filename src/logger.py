b9eaee847b90ec98e2190820c849b2900030bb06
Abhinavexists <abhinavkumarsingh2023@gmail.com>
2025-10-04 23:29:58 +0530

    updated logger.py -> no seperate directory per log run

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,18 @@
-import os
 import sys
 import logging
 from datetime import datetime
 from pathlib import Path
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}"
-logs_path = Path.cwd() / 'logs' / LOG_FILE
-LOG_FILE_PATH = Path.joinpath(logs_path, LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+log_dir = Path.cwd() / 'logs'
+log_dir.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE_PATH = log_dir / LOG_FILE
+
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler(LOG_FILE_PATH),
         logging.StreamHandler(sys.stdout)
